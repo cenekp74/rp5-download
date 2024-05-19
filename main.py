8d096de7a277ce86dc2991dc30e1010ebed1f559
Čeněk Použník <potuznikcenek@gmail.com>
2024-05-19 20:01:08 +0200

    removed unused imports

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
